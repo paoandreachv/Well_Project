06c23287107bf8f64c2e19a32c45dc3fa63c72ad
Paola Chávez <paola.chavez@ug.uchile.cl>
2025-10-20 23:39:00 +0200

    feat: implementation of a wind rose

--- src/visualization.py
+++ src/visualization.py
@@ -3,7 +3,7 @@ import vtk
 def create_renderer():
     """ Creates and returns a VTK renderer """
     renderer = vtk.vtkRenderer()
-    renderer.SetBackground(1, 1, 1)  # White background
+    renderer.SetBackground(1.0, 1.0, 1.0)  # white background
     return renderer
 
 def create_render_window(renderer):
@@ -22,4 +22,58 @@ def create_interactor(render_window):
     interactor.SetInteractorStyle(style)
     
     return interactor
+
+def create_wind_rose(center, size=500):
+    """Creates a wind rose with  N, S, E, W lines and labels"""
+    cx, cy, cz = center
+    directions = {
+        "N": (0, size, 0),
+        "S": (0, -size, 0),
+        "E": (size, 0, 0),
+        "W": (-size, 0, 0)
+    }
+    
+    actors = []
+
+    # Create cardinal lines
+    for label, (dx, dy, dz) in directions.items():
+        line_source = vtk.vtkLineSource()
+        line_source.SetPoint1(cx, cy, cz)
+        line_source.SetPoint2(cx + dx, cy + dy, cz + dz)
+        
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(line_source.GetOutputPort())
+        
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().SetColor(0, 0, 0)  
+        actor.GetProperty().SetLineWidth(3)
+        actors.append(actor)
+        
+        # Create labels
+        text = vtk.vtkBillboardTextActor3D()
+        text.SetInput(label)
+        text.SetPosition(cx + dx * 1.1, cy + dy * 1.1, cz + dz * 1.05)
+        text.GetTextProperty().SetColor(0, 0, 0)
+        text.GetTextProperty().BoldOn()
+        text.GetTextProperty().SetFontSize(20)
+        actors.append(text)
+
+    # Circle at the center to highlight
+    circle = vtk.vtkRegularPolygonSource()
+    circle.SetCenter(cx, cy, cz)
+    circle.SetRadius(size * 0.1)
+    circle.SetNumberOfSides(50)
+    circle.Update()
+
+    circle_mapper = vtk.vtkPolyDataMapper()
+    circle_mapper.SetInputConnection(circle.GetOutputPort())
+
+    circle_actor = vtk.vtkActor()
+    circle_actor.SetMapper(circle_mapper)
+    circle_actor.GetProperty().SetColor(0, 0, 0)
+    circle_actor.GetProperty().SetOpacity(1)
+    actors.append(circle_actor)
+
+    return actors
 
